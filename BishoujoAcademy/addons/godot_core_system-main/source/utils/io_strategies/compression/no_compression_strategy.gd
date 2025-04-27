670dbe0847eadea76c442936bf77f431989ea06e
liukai <642430165@qq.com>
2025-04-27 15:02:23 +0800

    Revert "升级引擎到4.4，添加核心开发插件"
    This reverts commit a1b36ad941089802e6860ce0533ff05c25ba32ee.

--- BishoujoAcademy/addons/godot_core_system-main/source/utils/io_strategies/compression/no_compression_strategy.gd
+++
@@ -1,9 +0,0 @@
-extends "./compression_strategy.gd"
-
-## Returns the input bytes without modification.
-func compress(bytes: PackedByteArray) -> PackedByteArray:
-	return bytes
-
-## Returns the input bytes without modification.
-func decompress(bytes: PackedByteArray) -> PackedByteArray:
-	return bytes 
--- BishoujoAcademy/addons/godot_core_system-main/source/utils/io_strategies/encryption/no_encryption_strategy.gd
+++
@@ -1,9 +0,0 @@
-extends "./encryption_strategy.gd"
-
-## Returns the input bytes without modification.
-func encrypt(bytes: PackedByteArray, _key: PackedByteArray) -> PackedByteArray:
-	return bytes
-
-## Returns the input bytes without modification.
-func decrypt(bytes: PackedByteArray, _key: PackedByteArray) -> PackedByteArray:
-	return bytes 
